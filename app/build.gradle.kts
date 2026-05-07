00910644d8c6463517fb5a4f279612a7824df501
palinkiewicz <pogromca.ap@gmail.com>
2026-05-07 18:02:10 +0200

    Upgrade app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "pl.dakil.healthyshopping"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.3.0"
+        versionCode = 4
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
